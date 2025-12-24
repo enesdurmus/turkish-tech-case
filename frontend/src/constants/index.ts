558213bf860e675ca1d34d5b7725057ea3e74299
Enes Durmus <enes.durmus@spykegames.com>
2025-12-25 02:21:58 +0300

    fix: index

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -3,4 +3,4 @@ export const DRAWER_WIDTH = 240;
 export const DEFAULT_PAGE_SIZE = 5;
 export const PAGE_SIZE_OPTIONS = [5, 10, 20];
 
-export const DAYS_OF_WEEK_SHORT = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] as const;
+export const DAYS_OF_WEEK_SHORT = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] as const;
