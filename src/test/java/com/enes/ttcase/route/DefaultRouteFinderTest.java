e64d315335902818185f3d952b7fbd7b71d70faf
Enes Durmus <enes.durmus@spykegames.com>
2025-12-26 11:16:23 +0300

    feat: tests

--- src/test/java/com/enes/ttcase/route/DefaultRouteFinderTest.java
+++ src/test/java/com/enes/ttcase/route/DefaultRouteFinderTest.java
@@ -21,7 +21,7 @@ class DefaultRouteFinderTest {
         RouteFindContext context = new RouteFindContext(
                 mock(LocationDto.class),
                 mock(LocationDto.class),
-                Set.of(mock(TransportationDto.class))
+                Set.of()
         );
 
         // when
@@ -53,5 +53,57 @@ class DefaultRouteFinderTest {
         // then
         assertThat(actual).hasSize(1);
         assertThat(actual.getFirst().steps()).containsExactly(flightAB, busBC);
+    }
+
+    @Test
+    void findRoutes_whenNoFlightInRoute() {
+        // given
+        LocationDto locA = mock(LocationDto.class);
+        LocationDto locB = mock(LocationDto.class);
+        LocationDto locC = mock(LocationDto.class);
+
+        TransportationDto busAB = new TransportationDto(0L, locA, locB, TransportationType.BUS, Set.of(1), null, null);
+        TransportationDto busBC = new TransportationDto(1L, locB, locC, TransportationType.BUS, Set.of(1), null, null);
+
+        RouteFindContext context = new RouteFindContext(
+                locA,
+                locC,
+                Set.of(busAB, busBC)
+        );
+
+        // when
+        List<Route> actual = sut.findRoutes(context);
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void findRoutes_whenMoreThanMaxSteps() {
+        // given
+        LocationDto locA = mock(LocationDto.class);
+        LocationDto locB = mock(LocationDto.class);
+        LocationDto locC = mock(LocationDto.class);
+        LocationDto locD = mock(LocationDto.class);
+        LocationDto locE = mock(LocationDto.class);
+        LocationDto locF = mock(LocationDto.class);
+
+        TransportationDto flightAB = new TransportationDto(0L, locA, locB, TransportationType.FLIGHT, Set.of(1), null, null);
+        TransportationDto busBC = new TransportationDto(1L, locB, locC, TransportationType.BUS, Set.of(1), null, null);
+        TransportationDto busCD = new TransportationDto(2L, locC, locD, TransportationType.BUS, Set.of(1), null, null);
+        TransportationDto busDE = new TransportationDto(3L, locD, locE, TransportationType.BUS, Set.of(1), null, null);
+        TransportationDto busEF = new TransportationDto(4L, locE, locF, TransportationType.BUS, Set.of(1), null, null);
+
+        RouteFindContext context = new RouteFindContext(
+                locA,
+                locF,
+                Set.of(flightAB, busBC, busCD, busDE, busEF)
+        );
+
+        // when
+        List<Route> actual = sut.findRoutes(context);
+
+        // then
+        assertThat(actual).isEmpty();
     }
 }
